0c74591da2f2a7c672bc351f89013c6da824bd77
arjunvnair <arjunvnair@hotmail.com>
2019-12-21 18:37:07 -0600

    Add isSemanticsPreserving field to Transformation
    This will allow the Documenter to inform the user of whether or not their fuzz is semantics preserving.

--- src/main/kotlin/edu/illinois/cs/cs125/fuzzyjava/Transformations.kt
+++ src/main/kotlin/edu/illinois/cs/cs125/fuzzyjava/Transformations.kt
@@ -6,7 +6,12 @@ package edu.illinois.cs.cs125.fuzzyjava
 interface Transformation {
     val name: String // The type of transformation
     val arguments: Set<String> // A list of arguments for the transformation (size varies based on transformation type)
+    val isSemanticsPreserving: Boolean // Whether the transformation preserves semantics (i.e. the code functions the same)
 }
+
+// -- Semantics Preserving --
+
+// -- Non-Semantics Preserving --
 
 /**
  * Removes semicolons from the code.
@@ -15,7 +20,8 @@ interface Transformation {
  */
 class RemoveSemicolons(private val rand: Boolean) : Transformation {
     override val name: String = "remove-semicolons"
-    override val arguments: Set<String> = setOf<String>()
+    override val arguments = setOf<String>()
+    override val isSemanticsPreserving = false; // TODO: Change this to true if Fuzzy Java is ported to Fuzzy Kotlin
     init {
         if (rand == true) {
             arguments.plus("random")
